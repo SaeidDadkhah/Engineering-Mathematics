999b4a7075a0f404c9a8a802ff5936f73e63ba40
Dadkhah <9231066@ceit.local>
2017-01-14 19:33:56 +0330

    algorithm is classifying, need to set two thresholds!

--- Q03 - Face Recognition (SVD)/FaceRecognition.m
+++ Q03 - Face Recognition (SVD)/FaceRecognition.m
@@ -1,5 +1,5 @@
 %% init
-%{
+%
 clear
 
 %% 1. Read data set
@@ -7,10 +7,11 @@ address = 'Dataset/';
 
 inImage = ReadImage(sprintf('%s%s', address, '1-04.jpg'));
 M  = size(inImage(), 1) * size(inImage(), 2);
-N = 60;
+N = 180;
 A = zeros(M, N);
-for i = 1:10
-    for j = [4 5 6 7 11 13]
+Pics = [4 5 6 7 11 13];
+for i = 1:(N/size(Pics, 2))
+    for j = Pics;
         f = ReadImage(sprintf('%s%d-%02d.jpg', address, i, j));
         A(:, i) = f(:);
     end
@@ -22,25 +23,26 @@ A = A - meanOfA(:, ones(1, N));
 
 %% 3. SVD
 [u, s, v] = svd(A);
-R = size(s, 1);
-
-%% 4. Coordinate x_i
-xi = u(:, 1:10)' * A;
+R = size(s, 2);
 %}
 
+%% 4. Coordinate x_i
+xi = u(:, 1:R)' * A;
+
 %% 5. Choose threshold
+e1 = 9.5;
 
 %% 6. Projection
 efs = zeros(20, 1);
 
 ei = zeros(20, N);
 class = -1 * ones(N, 1);
-for i = 1:20
-    f = ReadImage(sprintf('Dataset/%d-11.jpg', i));
+for i = 1:50
+    f = ReadImage(sprintf('Dataset/%d-12.jpg', i));
     f = f(:) - meanOfA;
-    x = u(:, 1:10)' * f;
+    x = u(:, 1:size(Pics, 2))' * f;
     
-    ef = f - u(:, 1:10) * x;
+    ef = f - u(:, 1:size(Pics, 2)) * x;
     efs(i) = (ef' * ef) ^ .5;
     
     %% 7. Classify
@@ -49,6 +51,9 @@ for i = 1:20
         ei(i, j) = tmp' * tmp;
     end
     
+    subplot(10, 5, i);
+    plot(ei(i, :));
+    
 end
-plot(efs, '-o')
+%plot(efs, '-o')
 %[m, i] = min(tmp);
