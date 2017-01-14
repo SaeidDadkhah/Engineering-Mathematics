512b09bd1f70db52c30f7f1ce910a9ba35790dda
Dadkhah <9231066@ceit.local>
2017-01-14 16:41:55 +0330

    face recognition result seems true! NOT TESTED.

--- Q03 - Face Recognition (SVD)/FaceRecognition.m
+++ Q03 - Face Recognition (SVD)/FaceRecognition.m
@@ -1,9 +1,9 @@
 %% init
+%{
 clear
 
 %% 1. Read data set
 address = 'Dataset/';
-curDir = dir(address);
 
 inImage = ReadImage(sprintf('%s%s', address, '1-04.jpg'));
 M  = size(inImage(), 1) * size(inImage(), 2);
@@ -15,14 +15,6 @@ for i = 1:10
         A(:, i) = f(:);
     end
 end
-%{
-address = 'frontalimages_manuallyaligned_part1/b/';
-curDir = dir(address);
-for i = 1:(N/2)
-    f = ReadImage(sprintf('%s%s', address, curDir(i + 2).name));
-    A(:, i) = f(:);
-end
-%}
 
 %% 2. Normalize
 meanOfA = mean(A, 2);
@@ -33,17 +25,30 @@ A = A - meanOfA(:, ones(1, N));
 R = size(s, 1);
 
 %% 4. Coordinate x_i
-coordinateX = u(:, 1:R)' * A;
+xi = u(:, 1:10)' * A;
+%}
+
+%% 5. Choose threshold
 
 %% 6. Projection
 efs = zeros(20, 1);
+
+ei = zeros(20, N);
+class = -1 * ones(N, 1);
 for i = 1:20
     f = ReadImage(sprintf('Dataset/%d-11.jpg', i));
-    f = f(:);
-    x = u(:, 1:R)' * (f - meanOfA);
-
-    ef = f - meanOfA - u(:, 1:R) * x;
+    f = f(:) - meanOfA;
+    x = u(:, 1:10)' * f;
+    
+    ef = f - u(:, 1:10) * x;
     efs(i) = (ef' * ef) ^ .5;
+    
+    %% 7. Classify
+    for j = 1:N
+        tmp = xi(:, j) - x;
+        ei(i, j) = tmp' * tmp;
+    end
+    
 end
 plot(efs, '-o')
 %[m, i] = min(tmp);
--- Q03 - Face Recognition (SVD)/ReadImage.m
+++ Q03 - Face Recognition (SVD)/ReadImage.m
@@ -1,7 +1,7 @@
 function im = ReadImage(address)
 
 im = imread(address);
-%im = imresize(im, 0.3);
-im = im(1:6:size(im, 1), 1:6:size(im, 2), 1:size(im, 3));
+im = imresize(im, 0.2);
+%im = im(1:5:size(im, 1), 1:5:size(im, 2), 1:size(im, 3));
 im = rgb2gray(im);
 im = im2double(im);
